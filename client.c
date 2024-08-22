9019590b377fc9b6a1927951602713f2f88c5039
Marco Nobler <marco.nobler@gmail.com>
2024-08-22 10:31:28 +0200

    correzione porta

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+#define PORT 8088
 
 int main(int argc, char const* argv[])
 {
--- server.c
+++ server.c
@@ -6,7 +6,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+#define PORT 8088
+
 int main(int argc, char const* argv[])
 {
     int server_fd, new_socket;
